ab83ff18c9d3d37c089e55740a2a1821d9010c4d
idanya <danhilkau2@gmail.com>
2024-08-02 05:22:47 +0400

    feat: add sudo check, add-apt-repository check

--- sff.sh
+++ sff.sh
@@ -4,24 +4,37 @@
 DISTRO=$(grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
 VERSION=$(grep '^VERSION_ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
 
+# check if 'sudo' command is available
+if ! command -v sudo >/dev/null 2>&1; then
+    # if 'sudo' is not found, check if the user is not root
+    if [ "$(id -u)" -ne 0 ]; then
+        # print error message and exit if not root
+        echo "error: 'sudo' command is required but not available, and you are not root."
+        exit 1
+    else
+        # set alias for 'sudo' to an empty string if user is root
+        alias sudo=''
+    fi
+fi
+
 # determine installation method based on distribution and version
 if [ "$DISTRO" = "ubuntu" ]; then
     case "$VERSION" in
-        "20.04"|"18.04"|"16.04")
-            INSTALL_METHOD="direct"
-            ;;
-        *)
-            INSTALL_METHOD="ppa"
-            ;;
+    "20.04" | "18.04" | "16.04")
+        INSTALL_METHOD="direct"
+        ;;
+    *)
+        INSTALL_METHOD="ppa"
+        ;;
     esac
 elif [ "$DISTRO" = "debian" ]; then
     case "$VERSION" in
-        "12"|"11"|"10"|"9"|"8")
-            INSTALL_METHOD="direct"
-            ;;
-        *)
-            INSTALL_METHOD="apt"
-            ;;
+    "12" | "11" | "10" | "9" | "8")
+        INSTALL_METHOD="direct"
+        ;;
+    *)
+        INSTALL_METHOD="apt"
+        ;;
     esac
 else
     INSTALL_METHOD="direct"
@@ -34,6 +47,13 @@ if [ "$INSTALL_METHOD" = "direct" ]; then
 else
     # add ppa for ubuntu 22.04 or newer
     if [ "$INSTALL_METHOD" = "ppa" ]; then
+        # check if the 'add-apt-repository' command is available
+        if ! command -v add-apt-repository >/dev/null 2>&1; then
+            # print a message if 'add-apt-repository' command is not found
+            echo "'add-apt-repository' command is not available."
+            exit 1
+        fi
+
         sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
     fi
     # install fastfetch from ppa / directly (debian 13 or newer)
